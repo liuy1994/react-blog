642147520692541eb5d262b798ff702fbb6386c7
taohua <max.liu@xuantonginfo.com>
2018-10-24 20:04:13 +0800

    run eject

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rules: [{
+    test: /\.less$/,
+    use: [{
+      loader: 'style-loader',
+    }, {
+      loader: 'css-loader',
+    }, {
+      loader: 'less-loader',
+      options: {
+      modifyVars: {
+        'primary-color': '#1DA57A',
+        'link-color': '#1DA57A',
+        'border-radius-base': '2px',
+      },
+      javascriptEnabled: true,
+    },
+    }],
+  }]
+}
