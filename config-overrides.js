08e13bb2b0ee5f016a6c66d7c6f964a4d8abc1fb
taohua <max.liu@xuantonginfo.com>
2018-10-24 19:59:26 +0800

    add antd-css

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
 const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
     config
   )
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {"@primary-color": "#260"},
+    javascriptEnabled: true,
+  }) (config, env);
   return config
 }
