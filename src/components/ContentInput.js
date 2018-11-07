d4b7bbf4ab48f734f67cc19b198b47394d0f63cd
taohua <max.liu@xuantonginfo.com>
2018-11-07 09:53:07 +0800

    配置babel已解决自定义富文本编辑器

--- src/components/ContentInput.js
+++ src/components/ContentInput.js
@@ -7,8 +7,26 @@ class ContentInput extends Component{
     super(props)
     this.state = { text: '' } // You can also pass a Quill Delta here
   }
+  modules = {
+    toolbar: {
+      container: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+      ]
+    }
+  }
+  // formats = [
+  //   'header',
+  //   'bold', 'italic', 'underline', 'strike', 'blockquote',
+  //   'list', 'bullet', 'indent',
+  //   'link', 'image'
+  // ]
   cleanContent() {
     this.setState({ text: '' })
+    console.log('image')
   }
   imageHandler() {
     console.log('image')
@@ -16,14 +34,19 @@ class ContentInput extends Component{
   handleChange(value) {
     this.setState({ text: value })
   }
+
   render() {
     let { onInput, content } = this.props
     return (
-      <ReactQuill
-        value={content}
-        height="300"
-        placeholder='Compose an epic...'
-        onChange={onInput} />
+      <div className="content-input">
+        <ReactQuill theme="snow"
+                    modules={this.modules}
+                    value={content}
+                    height="300"
+                    placeholder='Compose an epic...'
+                    onChange={onInput}>
+        </ReactQuill>
+      </div>
     )
   }
 }
--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -53,16 +53,6 @@ class ContentList extends Component {
       headers: {
         authorization: 'authorization-text',
       },
-      onChange(info) {
-        if (info.file.status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-          message.error(`${info.file.name} file upload failed.`);
-        }
-      },
       beforeUpload(file){
         request.upload(file)
         return false
