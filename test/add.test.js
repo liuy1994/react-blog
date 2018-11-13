644c885b527a416c5bb09e205df412e1c9388eeb
taohua <max.liu@xuantonginfo.com>
2018-11-13 17:23:11 +0800

    开始mocha测试

---
+++ test/add.test.js
@@ -0,0 +1,14 @@
+let add = require('./add.js')
+let expect = require('chai').expect
+
+describe('测试', () => {
+  it('测试在5s后结束', (done) => {
+    let x = true
+    let f = () => {
+      x = false
+      expect(x).to.be.not.ok
+      done()
+    }
+    setTimeout(f, 3000);
+  })
+})
